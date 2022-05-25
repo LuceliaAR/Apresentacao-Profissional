0ab4392f497be4fbee28825ac84438badd4d6561
Lucelia <lucelia1976@gmail.com>
2022-05-24 21:16:55 -0300

    atualização HTML CSS e JS

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
-}
-);
+});
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-    }
-    )
-}
-)
+    })
+})
